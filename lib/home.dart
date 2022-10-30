81d6cdb6e1c35fd6cdc74b34220809e324f3f32e
Mica Angela Bacala <bacalamica.angela718@gmail.com>
2022-10-30 22:31:18 +0800

    Bacala Pushing

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:innovativetaskgroupone/memberTab/tabbacala.dart';
 import 'package:innovativetaskgroupone/memberTab/tabbracho.dart';
 import 'package:innovativetaskgroupone/memberTab/tablosbanos.dart';
 
@@ -57,7 +58,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
       body: TabBarView(
         controller: tabController,
         children: const [
-          TabNaval(),
+          TabBacala(),
           TabBracho(),
           TabNaval(),
           TabNaval(),
